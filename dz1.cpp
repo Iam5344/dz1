f9e18a83f9543e056ddbd4ca5cf01121a2a137b0
Miyamoto <gleban228777@gmail.com>
2025-10-28 08:43:24 +0200

    Update dz1.cpp

--- dz1.cpp
+++ dz1.cpp
@@ -1,65 +1,106 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
-class String {
-private:
-    char* data;
-    size_t size;
-
+// Спадкування
+class Animal {
 public:
-    String() : data(nullptr), size(0) {
-        data = new char[1];
-        data[0] = '\0';
+    string name;
+    
+    Animal(string n) {
+        name = n;
     }
+    
+    void eat() {
+        cout << name << " ест" << endl;
+    }
+};
 
-    String(const char* str) {
-        if (str) {
-            size = 0;
-            while (str[size]) size++;
-            
-            data = new char[size + 1];
-            for (size_t i = 0; i <= size; i++) {
-                data[i] = str[i];
-            }
-        } else {
-            size = 0;
-            data = new char[1];
-            data[0] = '\0';
+class Dog : public Animal {
+public:
+    Dog(string n) : Animal(n) {}
+    
+    void bark() {
+        cout << name << " лает" << endl;
+    }
+};
+
+// Композиція
+class Engine {
+public:
+    int power;
+    
+    Engine(int p) {
+        power = p;
+    }
+    
+    void start() {
+        cout << "Двигатель запущен" << endl;
+    }
+};
+
+class Car {
+private:
+    Engine engine;
+    
+public:
+    string model;
+    
+    Car(string m, int p) : engine(p) {
+        model = m;
+    }
+    
+    void drive() {
+        engine.start();
+        cout << model << " едет" << endl;
+    }
+};
+
+// Агрегація
+class Teacher {
+public:
+    string name;
+    
+    Teacher(string n) {
+        name = n;
+    }
+};
+
+class University {
+private:
+    Teacher* teacher;
+    
+public:
+    string universityName;
+    
+    University(string n) {
+        universityName = n;
+        teacher = nullptr;
+    }
+    
+    void setTeacher(Teacher* t) {
+        teacher = t;
+    }
+    
+    void showTeacher() {
+        if (teacher != nullptr) {
+            cout << "Преподаватель: " << teacher->name << endl;
         }
     }
-
-    String() {
-        delete[] data;
-    }
-
-    // Перевантаження оператору []
-    char& operator[](size_t index) {
-        return data[index];
-    }
-
-    const char& operator[](size_t index) const {
-        return data[index];
-    }
-
-    friend ostream& operator<<(ostream& os, const String& str) {
-        if (str.data) {
-            os << str.data;
-        }
-        return os;
-    }
-
-    size_t length() const { return size; }
 };
 
 int main() {
-    setlocale(0,"");
-    String s("Hello");
+    Dog dog("Бобик");
+    dog.eat();
+    dog.bark();
     
-    cout << "s[0] = " << s[0] << endl;
-    cout << "s[1] = " << s[1] << endl;
+    Car car("Toyota", 150);
+    car.drive();
     
-    s[0] = 'h';
-    cout << "Після зміни: " << s << endl;
+    Teacher teacher("Иванов");
+    University university("КПИ");
+    university.setTeacher(&teacher);
+    university.showTeacher();
     
     return 0;
 }
